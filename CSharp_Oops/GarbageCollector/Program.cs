1d20681c508eb4c42c142a7ff3703edefb4320b0
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-28 16:31:30 +0530

    [Harshit] Add Garbage Collector

---
+++ CSharp_Oops/GarbageCollector/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GarbageCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
