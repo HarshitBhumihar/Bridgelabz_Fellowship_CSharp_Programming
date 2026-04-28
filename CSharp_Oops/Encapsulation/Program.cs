863311335f62a838ba0503baf02e7e082a4f0f71
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-28 12:48:45 +0530

    [Harshit] Add Encapsualtion practice

---
+++ CSharp_Oops/Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Practice e = new Practice();
+            e.setdata(22, "satyarth");
+            Console.WriteLine("name is " + e.getname());
+            Console.WriteLine("age is " + e.getage());
+
+        }
+    }
+}
