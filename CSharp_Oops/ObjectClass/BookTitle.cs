5670c75a4b0acb029236e680d43456f13e8ffa35
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-30 11:55:29 +0530

    [Harshit] Add Object class Code

--- CSharp_Oops/ObjectClass/BookTitle.cs
+++ CSharp_Oops/ObjectClass/BookTitle.cs
@@ -6,5 +6,20 @@ namespace ObjectClass
 {
     internal class BookTitle
     {
+        string title;
+        string author;
+        double price;
+        public BookTitle(string title, string author, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
     }
 }
--- CSharp_Oops/ObjectClass/Program.cs
+++ CSharp_Oops/ObjectClass/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //DisplayDetails d = new DisplayDetails(1, "Rohit", 18);
+            //d.display();
+            //TravelDetails t= new TravelDetails("Pune", "Mumbai", 150);
+            BookTitle b = new BookTitle("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+            b.display();
+
         }
     }
 }
