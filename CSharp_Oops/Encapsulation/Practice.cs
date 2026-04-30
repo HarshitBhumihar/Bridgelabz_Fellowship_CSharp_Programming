37ce90ed3344a8d9ca35df12b619290c4c94e043
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-04-30 11:49:12 +0530

    [Harshit] Add Encapsulation

---
+++ CSharp_Oops/Encapsulation/Practice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    internal class Practice
+    {
+        private int age;
+        private string name;
+        public void setdata(int a, string s)
+        {
+            age = a;
+            name = s;
+        }
+        public int getage()
+        {
+            return age;
+
+        }
+        public string getname()
+        {
+            return name;
+        }
+    }
+}
