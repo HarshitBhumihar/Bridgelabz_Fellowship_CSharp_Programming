758293b71ca117c02fabf88eecbc291b04190996
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:34:26 +0530

    [Harshit] Add HexDecimal Problem

---
+++ Advance_Programming/Regex/HexDecimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex4
+{
+    internal class HexDecimal
+    {
+        public static bool hexdecimal(String hex)
+        {
+            string pattern = @"^#[0-9a-zA-Z]{6}";
+            return Regex.IsMatch(hex, pattern);
+        }
+    }
+}
