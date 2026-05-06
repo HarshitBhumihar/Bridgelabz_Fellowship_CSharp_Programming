3863a97fdb97576ab42d59038f0c725e28ac2c76
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:32:43 +0530

    [Harshit] Add ExtractProgrammingLanguage Problem

---
+++ Advance_Programming/Regex/ExtractProgrammingLanguage.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex12
+//{
+//    class ExtractProgrammingLanguage
+//    {
+//        static void Main(string[] args)
+//        {
+//            string text = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+//            string pattern = @"\b(Java|Python|JavaScript|Go)\b";
+//            MatchCollection m= Regex.Matches(text, pattern);
+//            foreach (Match match in m)
+//            {
+//                Console.WriteLine(match.Value);
+//            }
+//        }
+//    }
+//}
