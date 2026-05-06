e634dbd22018a5e77f0d48fc321bf92a765691a1
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:23:20 +0530

    [Harshit] Add ExtractAllCapitalizedWords Problem

---
+++ Advance_Programming/Regex/ExtractAllCapitalizedWords.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex7
+//{
+//    class ExtractAllCapitalizedWords
+//    {
+//        public static void Main(String[] args)
+//        {
+//            string text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+//            string pattern = @"\b[A-Z][a-z]*\b";
+//            MatchCollection m = Regex.Matches(text, pattern);
+//            foreach (Match match in m)
+//            {
+//                Console.WriteLine(match.Value);
+//            }
+//        }
+//    }
+//}
