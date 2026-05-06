3d6af0db694d6ea9b732233535462e7201580196
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:43:05 +0530

    [Harshit] Add ValidIPAddress Problem

---
+++ Advance_Programming/Regex/ValidIPAddress.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex12
+//{
+//    class ValidIPAddress
+//    {
+//        static void Main(string[] args)
+//        {
+//            string ip = "192.168.1.1";
+
+//            string pattern = @"\b((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b";
+
+//            bool isValid = Regex.IsMatch(ip, pattern);
+
+//            Console.WriteLine(isValid);
+//        }
+//    }
+//}
