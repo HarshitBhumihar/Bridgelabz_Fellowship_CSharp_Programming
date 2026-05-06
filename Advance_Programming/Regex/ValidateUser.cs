54a532d53ada4c0373df621d2019777511276873
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:48:43 +0530

    [Harshit] Add ValidateUser Problem

---
+++ Advance_Programming/Regex/ValidateUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex2
+{
+    internal class ValidateUser
+    {
+        public static bool IsValidUsername(String name)
+        {
+            string pattern = @"^[a-zA-Z][a-zA-z0-9]{4-14}";
+            return Regex.IsMatch(name, pattern);
+        }
+    }
+}
