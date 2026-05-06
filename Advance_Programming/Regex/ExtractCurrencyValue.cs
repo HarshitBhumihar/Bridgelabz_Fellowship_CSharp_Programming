403d204f77d746e2a7029bbd727daed60648f018
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:26:48 +0530

    [Harshit] Add ExtractCurrencyValue Problem

---
+++ Advance_Programming/Regex/ExtractCurrencyValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex13 
+{
+    class ExtractCurrencyValue
+    {
+        static void Main(string[] args)
+        {
+            string text = "The total cost is $123.45 for the item.";
+            string pattern = @"\$\d+(\.\d{2})?";
+            Match match = Regex.Match(text, pattern);
+            if (match.Success)
+            {
+                Console.WriteLine("Extracted currency value: " + match.Value);
+            }
+        }
+    }
+}
