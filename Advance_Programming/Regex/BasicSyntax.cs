986945038b28249222285f03ca55675cc62b3f10
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:12:37 +0530

    [Harshit Singh] Add Basic Syntax of regex

---
+++ Advance_Programming/Regex/BasicSyntax.cs
@@ -0,0 +1,34 @@
+﻿using System;   
+using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+    internal class BasicSyntax
+    {
+        public static bool IsValidPhoneNumber(string phonenumber)
+        {
+            string pattern = @"^\d{10}$";
+            return Regex.IsMatch(phonenumber, pattern);  
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
