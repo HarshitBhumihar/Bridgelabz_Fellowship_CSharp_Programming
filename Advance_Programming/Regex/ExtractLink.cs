83ceed3ac2796250938fb5b675c3ec3fa0e3b9e0
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:31:14 +0530

    [Harshit] Add ExtractLink Problem

---
+++ Advance_Programming/Regex/ExtractLink.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex9
+//{
+//    internal class ExtractLink
+//    {
+//        static void Main(string[] args)
+//        {
+//            string text = "Visit our website at https://www.example.com or http://www.test.com for more information.";
+//            string pattern = @"https?://[^\s]+";
+//            MatchCollection m = Regex.Matches(text, pattern);
+//            foreach (Match m2 in m)
+//            {
+//                Console.WriteLine(m2.Value);
+//            }
+//        }
+//    }
+//}
