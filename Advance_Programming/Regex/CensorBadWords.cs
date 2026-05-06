88efdf995121cb8fd62f0bf667ce575bfc7e7eb2
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:18:21 +0530

    [Harshit] Add CensorBadWords Problem

---
+++ Advance_Programming/Regex/CensorBadWords.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex11
+//{
+//    class CensorBadWords
+//    {
+//        static void Main(string[] args)
+//        {
+//            string text = "This is a damn bad example with some stupid words.";
+//            string pattern = @"\b(damn|stupid|bad)\b";
+//            string final = Regex.Replace(text, pattern, "****");
+//            Console.WriteLine(final);
+//        }
+//    }
+//}
