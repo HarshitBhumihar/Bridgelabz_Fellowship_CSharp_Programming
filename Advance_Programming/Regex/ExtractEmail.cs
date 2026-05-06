84b8c23164937cfb73424912f27df30829cdb8fb
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:29:33 +0530

    [Harshit] Add ExtractEmail Problem

---
+++ Advance_Programming/Regex/ExtractEmail.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex6
+//{
+//    class ExtractEmail
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name= "Contact us at support@example.com and info@company.org";
+//            string pattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+//            MatchCollection matches = Regex.Matches(name, pattern);
+//            foreach(Match match in matches)
+//            {
+//                Console.WriteLine(match.Value);
+//            }
+            
+//        }
+//    }
+//}
