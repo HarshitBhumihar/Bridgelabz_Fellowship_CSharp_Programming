1cbdea6dc943737b416eab0dba011d85a04d75df
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:38:29 +0530

    [Harshit] Add ReplaceAndModify Problem

---
+++ Advance_Programming/Regex/ReplaceAndModify.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Text.RegularExpressions;
+
+//namespace Regex10
+//{
+//    internal class ReplaceAndModify
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input = "The quick brown       fox jumps over the lazy dog.";
+//            string pattern = @"\s+";
+           
+//            string result = Regex.Replace(input, pattern," ");
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
