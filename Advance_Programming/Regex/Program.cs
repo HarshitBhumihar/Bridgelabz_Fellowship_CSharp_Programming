d6c35cc60e92b3a5f4aed43f0118ec7de724fd6e
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:50:39 +0530

    [Harshit] Add Ptogram.cs file

---
+++ Advance_Programming/Regex/Program.cs
@@ -0,0 +1,33 @@
+﻿//using Regex2;
+//using Regex3;
+//using RegexExample;
+//using System;
+//using System.ComponentModel;
+//using System.Diagnostics.Metrics;
+//using Regex4;
+//namespace Regex1
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //string phonenumber = "1234567890";
+//            //bool result = BasicSyntax.IsValidPhoneNumber(phonenumber);
+//            //string name = "User_123";
+//            //bool result = ValidateUser.IsValidUsername(name);
+//            //string licencePlateNumber = "AB1234";
+//            //bool result= LicencePlateNumber.IsValidLicencePlateNumber(licencePlateNumber);
+//            //string hex = "#125AAB";
+//            //bool result= HexDecimal.hexdecimal(hex);
+            
+//            if (result)
+//                {
+//                Console.WriteLine("number is valid");
+//            }
+//            else
+//            {
+//                Console.WriteLine("number is invalid");
+//            }
+//        }
+//    }
+//}
