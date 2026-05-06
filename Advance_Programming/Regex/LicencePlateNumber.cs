4c957e30fb8ea421362fd3e70144b158fa3eefab
harshitbhumihar <harshitsinghmrzp2005@gmail.com>
2026-05-06 12:36:04 +0530

    [Harshit] Add LicencePlateNumber Problem

---
+++ Advance_Programming/Regex/LicencePlateNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex3
+{
+    internal class LicencePlateNumber
+    {
+        public static bool IsValidLicencePlateNumber(string licencePlateNumber)
+        {
+            string pattern = @"^[A-Z]{2}[0-9]{4}$";
+            return Regex.IsMatch(licencePlateNumber, pattern);
+
+        }
+        }
+}
